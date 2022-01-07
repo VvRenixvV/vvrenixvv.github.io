93175fb417e556737ac882986e534281f7c94ec1
VvRenixvV <57291253+VvRenixvV@users.noreply.github.com>
2022-01-07 03:52:34 +0000

    Modals Created, Started Filling Content

--- static/open_modal.js
+++ static/open_modal.js
@@ -1,31 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Get the modal
-  var modal = document.getElementById("myModal");
+  var modals = document.querySelectorAll(".modal");
 
   // Get the button that opens the modal
-  var divs = document.getElementsByClassName("panel");
+  var panels = document.querySelectorAll(".panel");
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  var spans = document.querySelectorAll(".close");
 
   // When the user clicks on the button, open the modal
-  for (i = 0; i < divs.length; i++) {
-    divs[i].addEventListener("click", function () {
-      modal.style.display = "block";
-      }
-    )
-  }
+  panels.forEach(panel => {
+    panel.addEventListener("click", function() {
+      modals.forEach(modal => {
+        var mod_id = modal.getAttribute('id');
+        var pan_id = panel.getAttribute('id');
+        if (mod_id.includes(pan_id)) {
+          modal.style.display = "block";
+          return;
+        }
+      })
+    })
+    }
+  )
 
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+  spans.forEach(span => {
+    span.addEventListener("click", function () {
+      modals.forEach(modal => {
+        modal.style.display = "none";
+      })
+    })
+  })
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+      modals.forEach(modal => {
+        if (event.target == modal) {
+          modal.style.display = "none";
+          return;
+        }
+    })
   }
 
 

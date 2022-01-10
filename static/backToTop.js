4618509f1723cd65e866b72821c92464c8f1c4f0
VvRenixvV <57291253+VvRenixvV@users.noreply.github.com>
2022-01-10 03:04:28 +0000

    Fixed Back To Top JS Error

--- static/backToTop.js
+++ static/backToTop.js
@@ -1,16 +1,17 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
+document.addEventListener("DOMContentLoaded", function() {
+  //Get the button:
+  mybutton = document.querySelector("#myBtn");
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
   }
-}
+})
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {

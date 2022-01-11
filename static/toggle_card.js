8237bd784cd57dc44aaa26e33deebd626a3337b5
VvRenixvV <57291253+VvRenixvV@users.noreply.github.com>
2022-01-11 08:41:01 +0000

    Expand All Button + Alert Bar

--- static/toggle_card.js
+++ static/toggle_card.js
@@ -6,13 +6,42 @@ window.addEventListener('load', function () {
   cardTitles.forEach(title => {
     title.addEventListener("click", function () {
       this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
+      var card = this.nextElementSibling;
+      if (card.style.display === "block") {
+        card.style.display = "none";
       } else {
-        content.style.display = "block";
+        card.style.display = "block";
       }
     })
   })
 
+  var allCards = this.document.querySelectorAll(".card");
+  var expandAllBtn = this.document.querySelector("#expandAllBtn");
+  expandAllBtn.addEventListener("click", function () {
+    if (expandAllBtn.innerHTML == "Expand All") {
+      changeShadows();
+      allCards.forEach(card => {
+        card.style.display = "block";
+      })
+      cardTitles.forEach(title => {
+        title.classList.add("active");
+      })
+      expandAllBtn.innerHTML = "Collapse All";
+    } else {
+      allCards.forEach(card => {
+        card.style.display = "none";
+      })
+      cardTitles.forEach(title => {
+        title.classList.remove("active");
+      })
+      expandAllBtn.innerHTML = "Expand All";
+    }
+  })
+
+  var colors = ["var(--green)", "var(--dark-green)", "var(--orange)", "var(--yellow)", "var(--ming)"];
+  function changeShadows() {
+    allCards.forEach(card => {
+      card.style.boxShadow = "5px 5px " + colors[Math.floor(Math.random() * colors.length)];
+    })
+  }
 })

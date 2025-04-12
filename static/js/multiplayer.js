5a86a5d452ce78dc9b4c79fc5e7d15e6c1a46c9f
Rahul Rai <rairahul19rr@gmail.com>
2025-04-12 20:44:11 +0400

    Added new content and styling to Multiplayer pages

--- static/js/multiplayer.js
+++ static/js/multiplayer.js
@@ -1,7 +1,7 @@
 const xpCards = document.querySelectorAll('.xp-card'); 
 
 xpCards.forEach(card => { 
-    card.addEventListener('click', ()=> { 
+    card.addEventListener('click', (event) => { 
         // toggle open cards
         if (card.classList.contains('active')) {
             card.classList.remove('active');
@@ -17,4 +17,15 @@ function closeAllCards() {
     xpCards.forEach(card => { 
         card.classList.remove('active'); 
     }
-)}
+    )
+}
+
+
+const xpBriefHeaders = document.querySelectorAll('.xp-brief-header');
+
+xpBriefHeaders.forEach(brief => {
+    brief.addEventListener('click', (event) => {
+        console.log("Stop Propagation")
+        event.stopPropagation(); // Prevent the click from bubbling up
+    })
+})
